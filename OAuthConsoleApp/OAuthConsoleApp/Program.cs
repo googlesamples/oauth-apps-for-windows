268f05cdceb6faab35b29578f24010586acda6a9
William Denniss <will@icog.net>
2016-07-01 23:10:54 -0700

    Added a Console (CLI) sample app.

---
+++ OAuthConsoleApp/OAuthConsoleApp/Program.cs
@@ -0,0 +1,296 @@
+﻿// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+
+namespace OAuthConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+-----------------------+");
+            Console.WriteLine("|  Sign in with Google  |");
+            Console.WriteLine("+-----------------------+");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to sign in...");
+            Console.ReadKey();
+
+            Program p = new Program();
+            p.doOAuth();
+
+            Console.ReadKey();
+        }
+
+        // client configuration
+        const string clientID = "581786658708-elflankerquo1a6vsckabbhn25hclla0.apps.googleusercontent.com";
+        const string clientSecret = "3f6NggMbPtrmIBpgx-MK2xXK";
+        const string authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+        const string tokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
+        const string userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+        // ref http://stackoverflow.com/a/3978040
+        public static int GetRandomUnusedPort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+
+        private async void doOAuth()
+        {
+            // Generates state and PKCE values.
+            string state = randomDataBase64url(32);
+            string code_verifier = randomDataBase64url(32);
+            string code_challenge = base64urlencodeNoPadding(sha256(code_verifier));
+            const string code_challenge_method = "S256";
+
+            // Creates a redirect URI using an available port on the loopback address.
+            string redirectURI = string.Format("http://{0}:{1}/", IPAddress.Loopback, GetRandomUnusedPort());
+            output("redirect URI: " + redirectURI);
+
+            // Creates an HttpListener to listen for requests on that redirect URI.
+            var http = new HttpListener();
+            http.Prefixes.Add(redirectURI);
+            output("Listening..");
+            http.Start();
+
+            // Creates the OAuth 2.0 authorization request.
+            string authorizationRequest = string.Format("{0}?response_type=code&scope=openid%20profile&redirect_uri={1}&client_id={2}&state={3}&code_challenge={4}&code_challenge_method={5}",
+                authorizationEndpoint,
+                System.Uri.EscapeDataString(redirectURI),
+                clientID,
+                state,
+                code_challenge,
+                code_challenge_method);
+
+            // Opens request in the browser.
+            System.Diagnostics.Process.Start(authorizationRequest);
+
+            // Waits for the OAuth authorization response.
+            var context = await http.GetContextAsync();
+
+            // Brings the Console to Focus.
+            BringConsoleToFront();
+
+            // Sends an HTTP response to the browser.
+            var response = context.Response;
+            string responseString = string.Format("<html><head><meta http-equiv='refresh' content='10;url=https://google.com'></head><body>Please return to the app.</body></html>");
+            var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            var responseOutput = response.OutputStream;
+            Task responseTask = responseOutput.WriteAsync(buffer, 0, buffer.Length).ContinueWith((task) =>
+            {
+                responseOutput.Close();
+                http.Stop();
+                Console.WriteLine("HTTP server stopped.");
+            });
+
+            // Checks for errors.
+            if (context.Request.QueryString.Get("error") != null)
+            {
+                output(String.Format("OAuth authorization error: {0}.", context.Request.QueryString.Get("error")));
+                return;
+            }
+            if (context.Request.QueryString.Get("code") == null
+                || context.Request.QueryString.Get("state") == null)
+            {
+                output("Malformed authorization response. " + context.Request.QueryString);
+                return;
+            }
+
+            // extracts the code
+            var code = context.Request.QueryString.Get("code");
+            var incoming_state = context.Request.QueryString.Get("state");
+
+            // Compares the receieved state to the expected value, to ensure that
+            // this app made the request which resulted in authorization.
+            if (incoming_state != state)
+            {
+                output(String.Format("Received request with invalid state ({0})", incoming_state));
+                return;
+            }
+            output("Authorization code: " + code);
+
+            // Starts the code exchange at the Token Endpoint.
+            performCodeExchange(code, code_verifier, redirectURI);
+        }
+
+        async void performCodeExchange(string code, string code_verifier, string redirectURI)
+        {
+            output("Exchanging code for tokens...");
+
+            // builds the  request
+            string tokenRequestURI = "https://www.googleapis.com/oauth2/v4/token";
+            string tokenRequestBody = string.Format("code={0}&redirect_uri={1}&client_id={2}&code_verifier={3}&client_secret={4}&scope=&grant_type=authorization_code",
+                code,
+                System.Uri.EscapeDataString(redirectURI),
+                clientID,
+                code_verifier,
+                clientSecret
+                );
+
+            // sends the request
+            HttpWebRequest tokenRequest = (HttpWebRequest)WebRequest.Create(tokenRequestURI);
+            tokenRequest.Method = "POST";
+            tokenRequest.ContentType = "application/x-www-form-urlencoded";
+            tokenRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            byte[] _byteVersion = Encoding.ASCII.GetBytes(tokenRequestBody);
+            tokenRequest.ContentLength = _byteVersion.Length;
+            Stream stream = tokenRequest.GetRequestStream();
+            await stream.WriteAsync(_byteVersion, 0, _byteVersion.Length);
+            stream.Close();
+
+            try
+            {
+                // gets the response
+                WebResponse tokenResponse = await tokenRequest.GetResponseAsync();
+                using (StreamReader reader = new StreamReader(tokenResponse.GetResponseStream()))
+                {
+                    // reads response body
+                    string responseText = await reader.ReadToEndAsync();
+                    Console.WriteLine(responseText);
+
+                    // converts to dictionary
+                    Dictionary<string, string> tokenEndpointDecoded = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseText);
+
+                    string access_token = tokenEndpointDecoded["access_token"];
+                    userinfoCall(access_token);
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError)
+                {
+                    var response = ex.Response as HttpWebResponse;
+                    if (response != null)
+                    {
+                        output("HTTP: " + response.StatusCode);
+                        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            // reads response body
+                            string responseText = await reader.ReadToEndAsync();
+                            output(responseText);
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+        async void userinfoCall(string access_token)
+        {
+            output("Making API Call to Userinfo...");
+
+            // builds the  request
+            string userinfoRequestURI = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+            // sends the request
+            HttpWebRequest userinfoRequest = (HttpWebRequest)WebRequest.Create(userinfoRequestURI);
+            userinfoRequest.Method = "GET";
+            userinfoRequest.Headers.Add(string.Format("Authorization: Bearer {0}", access_token));
+            userinfoRequest.ContentType = "application/x-www-form-urlencoded";
+            userinfoRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+
+            // gets the response
+            WebResponse userinfoResponse = await userinfoRequest.GetResponseAsync();
+            using (StreamReader userinfoResponseReader = new StreamReader(userinfoResponse.GetResponseStream()))
+            {
+                // reads response body
+                string userinfoResponseText = await userinfoResponseReader.ReadToEndAsync();
+                output(userinfoResponseText);
+            }
+        }
+
+        /// <summary>
+        /// Appends the given string to the on-screen log, and the debug console.
+        /// </summary>
+        /// <param name="output">string to be appended</param>
+        public void output(string output)
+        {
+            Console.WriteLine(output);
+        }
+
+        /// <summary>
+        /// Returns URI-safe data with a given input length.
+        /// </summary>
+        /// <param name="length">Input length (nb. output will be longer)</param>
+        /// <returns></returns>
+        public static string randomDataBase64url(uint length)
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] bytes = new byte[length];
+            rng.GetBytes(bytes);
+            return base64urlencodeNoPadding(bytes);
+        }
+
+        /// <summary>
+        /// Returns the SHA256 hash of the input string.
+        /// </summary>
+        /// <param name="inputStirng"></param>
+        /// <returns></returns>
+        public static byte[] sha256(string inputStirng)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(inputStirng);
+            SHA256Managed sha256 = new SHA256Managed();
+            return sha256.ComputeHash(bytes);
+        }
+
+        /// <summary>
+        /// Base64url no-padding encodes the given input buffer.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static string base64urlencodeNoPadding(byte[] buffer)
+        {
+            string base64 = Convert.ToBase64String(buffer);
+
+            // Converts base64 to base64url.
+            base64 = base64.Replace("+", "-");
+            base64 = base64.Replace("/", "_");
+            // Strips padding.
+            base64 = base64.Replace("=", "");
+
+            return base64;
+        }
+
+        // Hack to bring the Console window to front.
+        // ref: http://stackoverflow.com/a/12066376
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public void BringConsoleToFront()
+        {
+            SetForegroundWindow(GetConsoleWindow());
+        }
+
+    }
+}
---
+++ OAuthConsoleApp/README.md
@@ -0,0 +1,78 @@
+OAuth for Apps: Sample Console Application for Windows
+======================================================
+
+This sample shows how to do an OAuth 2.0 Authorization flow from a Windows
+Console application.
+
+Introduction
+------------
+
+When doing an OAuth 2.0 Authorization flow in a native application, it is 
+important to follow best practices, which require using the browser (and not 
+an embedded browser).
+
+This sample demonstrates how you can open the user's browser with your OAuth 2.0
+authorization request (where they might already be logged in!), have them
+complete the consent, recieve the Authorization Code using a local loopback
+socket, and exchanging that code for authorization tokens.
+
+Pre-requisites
+--------------
+
+The protocols referenced in this sample are documented here:
+
+- [OAuth 2.0](https://developers.google.com/identity/protocols/OAuth2)
+- [Using OAuth 2.0 for Mobile and Desktop Applications](https://developers.google.com/identity/protocols/OAuth2InstalledApp)
+
+Getting Started
+---------------
+
+1. Open the solution file: `OAuthConsoleApp.sln`
+2. Run the app.
+3. When the app starts, tap any key and go through the flow.
+4. Tap any key to exit.
+
+
+Using your own credentials
+--------------------------
+
+The Sample comes backed with some demo client credentials, which are fine for
+testing, but make sure you use your own credentials before releasing any app,
+or sharing it with friends.
+
+1. Visit the [Credentials page of the Developers Console](https://console.developers.google.com/apis/credentials?project=_)
+2. Create a new OAuth 2.0 client, select `Other`
+3. Copy the client id and client secret, and replace the values supplied in this
+   sample.
+
+
+Support
+-------
+
+If you have a question related to these samples, or Google OAuth in general,
+please ask on Stack Overflow with the `google-oauth` tag
+ https://stackoverflow.com/questions/tagged/google-oauth
+
+If you've found an error in this sample, please file an issue:
+https://github.com/googlesamples/oauth-apps-for-windows/issues
+
+Patches are encouraged, and may be submitted by forking this project and
+submitting a pull request through GitHub.
+
+
+License
+-------
+
+Copyright 2016 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
